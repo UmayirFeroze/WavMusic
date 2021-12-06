40b46361bba08fb96cb720318761a767f0de2776
Umayir Feroze <umayir10@gmail.com>
2021-12-06 21:18:39 +0530

    Navigation bar

--- client-side/web-app/src/components/NavigationBar.js
+++ client-side/web-app/src/components/NavigationBar.js
@@ -16,22 +16,49 @@ export default function NavigationBar(props) {
   //     Wav Music
   // </a>
   return (
-    <>
-      <Navbar variant='dark' bg="inherit" >
-        <Navbar.Brand href='/home' style={{color: colors.primary}}>
-            <img src={logo} width='36' height='28' className='d-inline-block align-text-top' alt='logo' />
-          WavMusic
-        </Navbar.Brand>
+    // <>
+    //   <Navbar collapseOnSelect variant='dark' bg="inherit" >
+    //     <Navbar.Brand href='/home' style={{color: colors.primary}}>
+    //         <img src={logo} width='36' height='28' className='d-inline-block align-text-top' alt='logo' />
+    //       WavMusic
+    //     </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-       
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='mr-auto'>
+    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+    //     <Navbar.Collapse id="basic-navbar-nav">
+    //       <Nav className='mr-auto'>
+    //         <Nav.Link className="text-white" href='/home'>Home</Nav.Link>
+    //         <Nav.Link className="text-white" href='/tracks'>Tracks</Nav.Link>
+    //         <Nav.Link className="text-white" href='/albums'>Albums</Nav.Link>
+    //         <Nav.Link className="text-white" href='/artists'>Artists</Nav.Link>
+    //       </Nav>
+
+    //       <Nav className='ml-auto'>
+    //         <Nav.Link className="text-white font-weight-bold Button-secondary mr-2" href='/home'>Sign in</Nav.Link>
+    //         <Nav.Link className="text-white font-weight-bold Button-primary ml-2" style={{backgroundColor: 'inherit'}} href='/tracks'>Sign up</Nav.Link>
+    //       </Nav>
+
+    //     </Navbar.Collapse>
+    //   </Navbar>
+    // </>
+    <Navbar bg='inherit' expand='lg'>
+      <Navbar.Brand href='/home' style={{color: colors.primary}}>
+           <img src={logo} width='36' height='28' className='d-inline-block align-text-top' alt='logo' />
+         WavMusic
+       </Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='mr-auto'>
             <Nav.Link className="text-white" href='/home'>Home</Nav.Link>
-            <Nav.Link className="text-white" href='/link'>Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </>
+            <Nav.Link className="text-white" href='/tracks'>Tracks</Nav.Link>
+            <Nav.Link className="text-white" href='/albums'>Albums</Nav.Link>
+            <Nav.Link className="text-white" href='/artists'>Artists</Nav.Link>
+        </Nav>
+        <Nav className='ml-auto' inline>
+            <Nav.Link className="text-white font-weight-bold Button-secondary mr-lg-2 px-3" href='/home'>Sign in</Nav.Link>
+            <Nav.Link className="text-white font-weight-bold Button-primary ml-lg-2 px-3" style={{backgroundColor: 'inherit'}} href='/tracks'>Sign up</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
--- client-side/web-app/src/screens/LandingScreen.js
+++ client-side/web-app/src/screens/LandingScreen.js
@@ -8,15 +8,14 @@ export default function LandingScreen(props) {
       <NavigationBar/>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
+
         <p>The one stop for all music</p>
+
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
+          className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
+        
       </header>
     </div>
   );
