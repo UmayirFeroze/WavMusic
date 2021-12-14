b4e20af7f144a84f8becc13722dada912480a186
Umayir Feroze <umayir10@gmail.com>
2021-12-14 15:36:58 +0530

    Code refactor

--- client-side/web-app/src/App.js
+++ client-side/web-app/src/App.js
@@ -1,65 +1,88 @@
-import { Container,Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import './App.css';
 
 import HomeScreen from './screens/HomeScreen';
 import colors from './config/colors';
 import logo from './logo.svg';
 import NavigationBar from './components/NavigationBar';
-import Footer from './components/AppFooter';
+import Footer from './components/screen/AppFooter';
 import MusicBar from './components/MusicBar';
 import { useEffect, useState } from 'react';
-import Header from './components/Header';
-import SideNavigation from './components/SideNavigation';
+import Header from './components/screen/Header';
+import SideNavigation from './components/screen/SideNavigation';
 import AppSettings from './components/appSettings';
 import Dashboard from './components/Dashboard';
 
 function App() {
-  const [loaded, setLoaded] = useState(false)
+  const [loaded, setLoaded] = useState(false);
   const [popular, setPopular] = useState([]);
 
   const songsInput = [
-      {id:1, name:'Diamonds', image:'Rihanna.png', authors: [{name:'Rihanna'}], album:'Loud', favourite:true}, 
-      {id:2, name:'Umbrella', image:'Rihanna.png', authors: [{name:'Rihanna', favourite:true}, {name:'Eminem'}], album:'Loud', favourite:false},
-      {id:3, name:'Right Now (Na na na)', image:'Akon.png', authors: [{name:'Akon'}], album:'Freedom', favourite:true},
-      {id:4, name:'Beautiful', image:'Akon.png', authors: [{name:'Akon'}], album:'Freedom', favourite:false},
-      {id:5, name:'Swalla', image:'Jason.jpeg', authors: [{name:'Jason Durelo'}], album:'2017', favourite:false},
-      {id:6, name:'Jalebi baby', image:'Jason.jpeg', authors: [{name:'Jason Durelo'}], album:'Just another series', favourite:true},
+    {
+      id: 1,
+      name: 'Diamonds',
+      image: 'Rihanna.png',
+      authors: [{ name: 'Rihanna' }],
+      album: 'Loud',
+      favourite: true,
+    },
+    {
+      id: 2,
+      name: 'Umbrella',
+      image: 'Rihanna.png',
+      authors: [{ name: 'Rihanna', favourite: true }, { name: 'Eminem' }],
+      album: 'Loud',
+      favourite: false,
+    },
+    {
+      id: 3,
+      name: 'Right Now (Na na na)',
+      image: 'Akon.png',
+      authors: [{ name: 'Akon' }],
+      album: 'Freedom',
+      favourite: true,
+    },
+    {
+      id: 4,
+      name: 'Beautiful',
+      image: 'Akon.png',
+      authors: [{ name: 'Akon' }],
+      album: 'Freedom',
+      favourite: false,
+    },
+    {
+      id: 5,
+      name: 'Swalla',
+      image: 'Jason.jpeg',
+      authors: [{ name: 'Jason Durelo' }],
+      album: '2017',
+      favourite: false,
+    },
+    {
+      id: 6,
+      name: 'Jalebi baby',
+      image: 'Jason.jpeg',
+      authors: [{ name: 'Jason Durelo' }],
+      album: 'Just another series',
+      favourite: true,
+    },
   ];
 
-  useEffect(()=>{
-      if(!loaded) {
-          setPopular(songsInput);
-          setLoaded(true);
-      }
-  },[popular,loaded])
-  
+  useEffect(() => {
+    if (!loaded) {
+      setPopular(songsInput);
+      setLoaded(true);
+    }
+  }, [popular, loaded]);
+
   return (
-    // <div className="sidebar-mini layout-fixed  layout-navbar-fixed dark-mode">
-    <div className='sidebar-mini control-sidebar-slide-open dark-mode layout-footer-fixed layout-fixed'>
-      
-      <div className='wrapper'>
-        <Header/>
-        <SideNavigation/>
-        <Dashboard/>
-        <Footer/>
-        {/* <AppSettings/> */}
-      </div>
+    <div className='wrapper'>
+      <Header />
+      <SideNavigation />
+      {/* <Dashboard/> */}
+      <Footer />
     </div>
-    
   );
 }
-{/* <Container className="App" fluid>
-<NavigationBar/>
-  <Container fluid style={{minHeight:'100vh'}} fluid>
-    <div className="mt-0 border text-left">
-      <h1 className='display-3 text-white'>Page Title</h1>
-    </div>
-    <MusicBar title="Popular" songs={popular && popular}/>
-    <MusicBar title="Trending" songs={popular && popular}/>
-    <MusicBar title="Hot Selling" songs={popular && popular}/>
-    <MusicBar title="Rabdom" songs={popular && popular}/>
-  </Container>
-  <Footer/>
-</Container> */}
 
 export default App;
--- client-side/web-app/src/components/Screen.js
+++ client-side/web-app/src/components/Screen.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import Footer from './Footer';
-import NavigationBar from './NavigationBar';
+
+import Footer from './screen/AppFooter';
+import Header from './screen/Header';
+import SideNavigation from './screen/SideNavigation';
+
 
 export default function Screen({ children, title, subtitle }) {
   return (
-    <Container className='App p-0' fluid >
-
-      <NavigationBar />
+    <div className='wrapper'>
+      <Header />
+      <SideNavigation />
       
-      {/* TODO: Header */}
-
-      {/* Screen Content */}
-      <Container fluid className='text-left h-100 overflow-hidden'>
-        <div className="mt-0 border">
-          <h1 className='display-3 text-white'>{title && title}</h1>
-          <h2 className='text-white'>{subtitle && subtitle}</h2>
-        </div>
-        
-        <Container fluid className="overflow-auto border" >
-          {children}
-        </Container>
-      </Container>
-
       <Footer />
-    </Container>
+    </div>
   );
 }
